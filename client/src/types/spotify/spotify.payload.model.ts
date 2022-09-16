954321dd027ddb825c2123d920b6b28283ebe0e2
Bruno Garcia <bruno.garcia@wallbox.com>
2022-09-16 20:51:03 +0200

    feat: zod - spotify payload

--- client/src/types/spotify/spotify.payload.model.ts
+++ client/src/types/spotify/spotify.payload.model.ts
@@ -1,10 +1,16 @@
+import { z } from 'zod';
+
 export enum SpotifyTimeRange {
   LONG_TERM = 'long_term',
   MEDIUM_TERM = 'medium_term',
   SHORT_TERM = 'short_term',
 }
 
-export interface SpotifyPayload  {
-  limit?: number;
-  time_range?: SpotifyTimeRange
-}
+const SpotifyTimeRangeSchema = z.nativeEnum(SpotifyTimeRange);
+
+const SpotifyPayloadSchema = z.object({
+  limit: z.number().optional(),
+  time_range: SpotifyTimeRangeSchema.optional(),
+});
+
+export type SpotifyPayload = z.infer<typeof SpotifyPayloadSchema>;
