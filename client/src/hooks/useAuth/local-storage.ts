e69700fc17d0babdcfe3609cfde32e12d8195917
Bruno Garcia <bruno.garcia@wallbox.com>
2022-10-13 15:18:47 +0200

    refactor: use auth

--- client/src/hooks/useAuth/local-storage.ts
+++ client/src/hooks/useAuth/local-storage.ts
@@ -1,5 +1,6 @@
 // Map for localStorage keys
-export const LOCALSTORAGE_KEYS: Record<string, string> = {
+export const LOCAL_STORAGE_KEYS: Record<string, string> = {
+  hasError: "spotify_has_error",
   accessToken: "spotify_access_token",
   refreshToken: "spotify_refresh_token",
   expireTime: "spotify_token_expire_time",
@@ -7,9 +8,9 @@ export const LOCALSTORAGE_KEYS: Record<string, string> = {
 };
 
 // Map to retrieve localStorage values
-export const LOCALSTORAGE_VALUES = {
-  accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
-  refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
-  expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
-  timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
+export const LOCAL_STORAGE_VALUES = {
+  accessToken: window.localStorage.getItem(LOCAL_STORAGE_KEYS.accessToken),
+  refreshToken: window.localStorage.getItem(LOCAL_STORAGE_KEYS.refreshToken),
+  expireTime: window.localStorage.getItem(LOCAL_STORAGE_KEYS.expireTime),
+  timestamp: window.localStorage.getItem(LOCAL_STORAGE_KEYS.timestamp),
 };
